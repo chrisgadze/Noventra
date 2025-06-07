abe956f199babc9e3df33b17724bf78bfd6dda64
Gadze Chris Selorm <gadzechrisselorm01@gmail.com>
2025-06-07 11:52:23 -0500

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
+  const loggedIn = {firstName : 'Chris', lastName : 'Gadze', email : 'gadzechris001@gmail.com'}
+  
   return (
-    <div>Home</div>
+    <section className='home'>
+     <div className = "home-content">
+      <header className=''>
+        <HeaderBox 
+        type ="greeting"
+        title ="Welcome!" 
+        user = {loggedIn?.firstName || "Guest"}
+        subtext = " Access and manage your account and transations efficiently"       
+        />
+
+        <TotalBalanceBox 
+        accounts = {[]}
+        totalBanks = {1}
+        totalCurrentBalance = {1250.45}
+      />
+      </header>
+      
+     </div>
+     <RightSidebar 
+          user = {loggedIn}
+          transations = {[]}
+          banks = {[{currentBalance: 123.4}, {currentBalance: 500}]}/>
+    </section>
   )
 }
 
